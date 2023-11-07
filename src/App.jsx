ce00824c33310fb351acd7cb9826c905c865e54e
Kumar <Anish.Kumar@insight.com>
2023-11-07 21:48:58 +0530

    Landing page and style added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import LandingPage from "./Pages/LandingPage";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <LandingPage />
+    </>
+  );
 }
 
 export default App;
